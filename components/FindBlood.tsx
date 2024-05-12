4c1d52baae8b2ddc78dbf26e0b0df199d94e0861
kishalaymodak <kishalaymodak@gmail.com>
2024-05-13 00:12:22 +0530

    space removed

--- components/FindBlood.tsx
+++ components/FindBlood.tsx
@@ -84,8 +84,13 @@ export default function FindBlood() {
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           onClick={async () => {
-            const bloodtype = bloodgroup.toUpperCase();
-            const UserData = await findBloodBank(bloodtype, city, state);
+            let bloodtype = bloodgroup.toUpperCase();
+            bloodtype = bloodtype.replace(/\s*$/, "");
+            const UserData = await findBloodBank(
+              bloodtype,
+              city.replace(/\s*$/, ""),
+              state.replace(/\s*$/, "")
+            );
 
             setData(UserData);
           }}
